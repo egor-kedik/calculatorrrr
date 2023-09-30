8e20ca9c670614788ea227ca5ac12d600cbb0682
Egor Kedik <Egor Kedik@DESKTOP-O3L18AR>
2023-09-30 21:23:10 +0300

    Добавьте файлы проекта.

---
+++ calculatorrrr/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            calculator();
+        }
+        static void calculator()
+        {
+            char operation;
+            double number1;
+            double number2;
+            onumber1(out number1);
+            onumber2(out number2);
+            inputoperation(out operation);
+            switch (operation)
+            {
+                case '+':
+                    Console.WriteLine(number1 + number2);
+                    break;
+                case '-':
+                    Console.WriteLine(number1 - number2);
+                    break;
+                case '*':
+                    Console.WriteLine(number1 * number2);
+                    break;
+                case '/':
+                    Console.WriteLine(number1 / number2);
+                    break;
+                default:
+                    Console.WriteLine("Операция не определена");
+                    break;
+            }
+        }
+        private static double onumber1(out double cnumber1)
+        {
+            Console.WriteLine("Введите первое число");
+            string snumber1 = Console.ReadLine();
+            if (double.TryParse(snumber1, out cnumber1))
+            {
+                return cnumber1;
+            }
+            else
+            {
+                Console.WriteLine("Ошибка, попробуйте снова");
+                onumber1(out cnumber1);
+                return cnumber1;
+            }
+
+        }
+        private static double onumber2(out double cnumber2)
+        {
+            Console.WriteLine("Введите второе число");
+            string snumber2 = Console.ReadLine();
+            if (double.TryParse(snumber2, out cnumber2))
+            {
+                return cnumber2;
+            }
+            else
+            {
+                Console.WriteLine("Ошибка, попробуйте снова");
+                onumber2(out cnumber2);
+                return cnumber2;
+            }
+
+        }
+        private static char inputoperation(out char coperation)
+        {
+            Console.WriteLine("Введите операцию, допустимые операции + - / *");
+            string snumber2 = Console.ReadLine();
+            if (char.TryParse(snumber2, out coperation))
+            {
+                if (coperation == '+' || coperation == '-' || coperation == '*' || coperation == '/')
+                    return coperation;
+                else
+                {
+                    Console.WriteLine("Ошибка, попробуйте снова");
+                    inputoperation(out coperation);
+                }
+            }
+
+            else
+            {
+                Console.WriteLine("Ошибка, попробуйте снова");
+                inputoperation(out coperation);
+                return coperation;
+            }
+            return coperation;
+        }
+
+    }
+}
